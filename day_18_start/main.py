e18e0530925bfeeb07ce1b9e7ead86135ed01f72
Alex England <80018085+meatsack-sudo@users.noreply.github.com>
2024-04-20 09:46:55 -0500

    commit progress

---
+++ day_18_start/main.py
@@ -0,0 +1,44 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("red")
+# tim.forward(100)
+# tim.right(90)
+
+for _ in range(4):
+    tim.forward(100)
+    tim.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
