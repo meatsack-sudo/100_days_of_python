7f8d15d816422187ca3d853c8b40d6eecfef8eec
Alex England <80018085+meatsack-sudo@users.noreply.github.com>
2024-06-03 20:26:03 -0500

    New days added

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,43 +1,19 @@
-import pandas
-
-phonetic_data = pandas.read_csv("NATO-alphabet-start\\nato_phonetic_alphabet.csv", index_col='letter')
-
-phonetic_alphabet = pandas.DataFrame.to_dict(phonetic_data)
-
-print(phonetic_alphabet['code']['A'])
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
-
-student_data_frame = pandas.DataFrame(student_dict)
-
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
-
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
+import pandas
+
+data = pandas.read_csv("C:/Users/Meatsack/Desktop/100_days_of_python/NATO-alphabet-start/nato_phonetic_alphabet.csv")
 #TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = "hi"
-
-list_of_letters = [letter for letter in user_input.upper()]
-print(list_of_letters)
-
-phonetic_dict = {key:value for (key, value) in phonetic_alphabet['code'].items() if key in list_of_letters}
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-# phonetic_dict = {phonetic_letter:letter for (phonetic_letter, letter) in phonetic_alphabet.values() if phonetic_data['code'].keys == letter in list_of_letters}
-# print(phonetic_dict)
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+word = input("Enter a word: ").upper()
+try: 
+    output_list = [phonetic_dict[letter] for letter in word]
+except KeyError:
+    print("Please only input english letters")
+else:    
+    print(output_list)
--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -3,9 +3,10 @@ from tkinter import messagebox
 import os
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 FONT_NAME = "Courier"
-FILE_DIR = os.path.expanduser("~/Desktop/100_days_of_python/password-manager-start/data.txt")
+FILE_DIR = os.path.expanduser("~/Desktop/100_days_of_python/password-manager-start/data.json")
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -31,28 +32,64 @@ def generate_password():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
-    #global FILE_DIR
-    if os.path.isfile(FILE_DIR) == False:
-        messagebox.showinfo(title="data.txt not found", message=f"Creating data.txt file at {FILE_DIR}. All login infomration will be stored here.")
-        with open(FILE_DIR, "w") as file:
-            file.write("Website | Email/Username | Password")
 
-    if os.path.isfile(FILE_DIR):
-        if len(website_entry.get()) < 3 or len(email_username_entry.get()) < 4 or len(password_entry.get()) < 4:
-            empty_check = messagebox.askokcancel(title="Short entries detected", message=f"One or more of your entered fields seem incomplete. Would you like to continue?\nWebsite: {website_entry.get()}\nEmail/Username: {email_username_entry.get()}\nPassword: {password_entry.get()}")
-            if empty_check:
-                with open(FILE_DIR, "a") as file:
-                    file.write("\n" + website_entry.get() + " | " + email_username_entry.get() + " | " + password_entry.get())
-                    website_entry.delete(0, END)
-                    password_entry.delete(0, END)
-        else:
-            is_ok = messagebox.askokcancel(title="Are you sure?", message=f"Would you like to save the following to data.txt? \n\nWebsite: {website_entry.get()}\nEmail/Username: {email_username_entry.get()}\nPassword: {password_entry.get()}")
-        
-            if is_ok:
-                with open(FILE_DIR, "a") as file:
-                    file.write("\n" + website_entry.get() + " | " + email_username_entry.get() + " | " + password_entry.get())
-                    website_entry.delete(0, END)
-                    password_entry.delete(0, END)
+    user_website = website_entry.get()
+    email = email_username_entry.get()
+    password = password_entry.get()
+
+    if not user_website or not email or not password:
+        messagebox.showwarning(title="Missing fields", message="Please fill out all fields.")
+        return
+
+    new_data = {
+        user_website: {
+            "email": email,
+            "password": password
+        }
+    }
+
+    #global FILE_DIR
+    if not os.path.isfile(FILE_DIR):
+        messagebox.showinfo(title="File not found", message=f"Creating {FILE_DIR}. All login infomration will be stored here.")
+        with open(FILE_DIR, "w") as file:
+            json.dump(new_data, file, indent=4)
+    else:
+        with open(FILE_DIR, "r") as file:
+            try:
+            #Reading old data
+                data = json.load(file)
+            #Updating old data with new data
+            except json.JSONDecodeError:
+                data = {}
+
+        data.update(new_data)
+
+        with open(FILE_DIR, "w") as file:
+            #Saving updated data
+            json.dump(data, file, indent=4)
+
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
+    #file.write("Website | Email/Username | Password")
+
+# ---------------------------- SEARCH SETUP ------------------------------- #
+
+def find_password():
+
+    try:
+        with open(FILE_DIR, "r") as file:
+            json_file = json.load(file)
+
+    except FileNotFoundError:
+        messagebox.showerror(title="File not found", message="Could not find a file with login information. Add login information and I'll create the file.")
+
+    website = website_entry.get()
+
+    if website_entry.get() in json_file:
+        messagebox.showinfo(title=f"{website.title()} log in information", message=f"Website: {website}\nEmail: {json_file[website]["email"]}\nPassword: {json_file[website]["password"]}")
+    else:
+        messagebox.showerror(title="No data found", message="No matching website found. Try your search again or add login information.")
+
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -62,34 +99,38 @@ window.config(padx=20, pady=20)
 
 #website label and entry
 website_label = Label(text="Website:", justify="left", font=(FONT_NAME, 10))
-website_entry = Entry(width=36)
+website_entry = Entry(width=30)
 website_entry.focus()
 
 
 website_label.grid(column=0, row=1, sticky="w")
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.grid(column=1, row=1, sticky="e")
 
 #email/username label and entry
 email_username_label = Label(text="Email/Username:", justify="left", font=(FONT_NAME, 10))
-email_username_entry = Entry(width=36)
+email_username_entry = Entry(width=50)
 
 email_username_label.grid(column=0, row=2, sticky="w")
-email_username_entry.grid(column=1, row=2, columnspan=2)
+email_username_entry.grid(column=1, row=2, columnspan=2, sticky="e")
 email_username_entry.insert(END, "alexerombou@gmail.com")
 
 #password label, entry, button
 password_label = Label(text="Password:", justify="left", font=(FONT_NAME, 10))
-password_entry = Entry(width=21)
-gen_password_button = Button(text="Generate Password", command=generate_password)
+password_entry = Entry(width=30)
+gen_password_button = Button(text="Generate Password", width=16, command=generate_password)
 
 password_label.grid(column=0, row=3, sticky="w")
-password_entry.grid(column=1, row=3)
-gen_password_button.grid(column=2, row=3)
+password_entry.grid(column=1, row=3, sticky="e")
+gen_password_button.grid(column=2, row=3, sticky="e")
 
 #add button
 add_button = Button(text="Add", width=36, command=save_password)
 
 add_button.grid(column=1, row=4, columnspan=2)
+
+#search button
+search_button = Button(text="Search", width=16, command=find_password)
+search_button.grid(column=2, row=1)
 
 # canvas for the logo
 canvas = Canvas(width=200, height=200)
